4becf4566d310b827708a7d6404d045c41af34ef
Manan Kapoor <23.kapoormanan@gmail.com>
2025-08-16 22:49:23 +0530

    Time: 2 ms (2.59%) | Memory: 7.8 MB (99.58%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int maximum69Number (int num) {
         vector<int> added;
-        int x;
-        int size = 0;
         int a = num ;
         while(a!=0){
-            x = a%10;
-            added.push_back(x);
-            size++;
-            a=a/10;
+            added.push_back(a%10);
+            a/=10;
         }
         reverse(added.begin(),added.end());
 
         for(int &number:added){
             if(number==6){
-                number = 9;
+                number= 9;
                 break;
             }
         }
-        int r =0;
+        int result=0;
         for(int number:added){
-            r = number+r*10;
+            result = result*10+number;
         }
-        return r;
+        return result;
         
     }
 };
