3a040ca5b79624e9a4ab080cd13d6a2cceed7c61
Manan Kapoor <23.kapoormanan@gmail.com>
2025-08-19 22:57:14 +0530

    Time: 47 ms (20.69%) | Memory: 64.2 MB (25.68%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -4,9 +4,16 @@ public:
         unordered_set<int> seen(nums.begin(),nums.end());
         int n = nums.size();
         vector<int> res={};
-        for(int i =1;i<=n;i++){
-            if(seen.find(i)==seen.end()){
-                res.push_back(i);
+        for(int i =0;i<n;i++){
+            int index = abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index]=-nums[index];
+            }            
+
+        }
+        for(int i =0;i<n;i++){
+            if(nums[i]>0){
+                res.push_back(i+1);
             }
         }
 
