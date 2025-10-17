fd7da1b7eb0cfbc3b0c37e8c973310074c7e3afc
Manan Kapoor <23.kapoormanan@gmail.com>
2025-10-18 00:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (23.40%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -24,8 +24,8 @@ public:
             pair<bool,int> p = make_pair(true,0);
             return p ;
         }
-        pair<int,int> left = isBalancedfast(root->left);
-        pair<int,int> right = isBalancedfast(root->right);
+        pair<bool,int> left = isBalancedfast(root->left);
+        pair<bool,int> right = isBalancedfast(root->right);
         
         bool leftans = left.first;
         bool rightans = right.first;
