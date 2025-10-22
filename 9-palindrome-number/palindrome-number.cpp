fad24a5d6fb4195943be056427b6698954425b91
Manan Kapoor <23.kapoormanan@gmail.com>
2025-10-22 22:44:38 +0530

    Time: 1 ms (59.27%) | Memory: 8.7 MB (41.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int rev = 0 ;
-        int digit ;
-        int org = x;
         if(x<0){
             return false;
         }
-        while(x>0){
-            digit = x%10;
-            if(rev>INT_MAX/10 || rev<INT_MIN/10){
-                return false;
-            }
-            rev = rev*10+digit;
-            x=x/10;
+        if(x>0 && x<10){
+            return true;
         }
-        return (org==rev);
-        
+        int digit ;
+        int org = x;
+        long rev = 0 ;
+        while(org>0){
+            digit = org%10;
+            if(rev>INT_MIN && rev<INT_MAX){
+                rev = rev*10 + digit;
+            }
+            org = org/10;
+        }
+        return (x==rev);
+
     }
 };
