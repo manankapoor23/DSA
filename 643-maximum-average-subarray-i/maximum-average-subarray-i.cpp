000f8bb7923bacf89bceb75084d77212b4d3b0dc
Manan Kapoor <23.kapoormanan@gmail.com>
2025-10-22 22:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 113.8 MB (11.27%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -3,19 +3,19 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         int sum = 0 ;
         for(int i =0;i<k;i++){
-            sum=sum+nums[i];
+            sum=sum+nums[i]; // sum of the first window 
         }
-        int maxsum = sum;
-        int s = 0 ;
-        int e = k;
+        int maxsum = sum; // set max sum to that sum
+        int s = 0 ; // left pointer 
+        int e = k; // right pointer 
         int size = nums.size();
         
         while(e<size){
-            sum = sum - nums[s];
-            s++;
-            sum = sum+nums[e];
+            sum = sum - nums[s]; // delete the left most 
+            s++; // move it ek aage 
+            sum = sum+nums[e]; // add ek right mai so window shift 
             e++;
-            maxsum = max(maxsum,sum);
+            maxsum = max(maxsum,sum); // max sum dekho and ussey return kro
         }
         return double(maxsum)/k;
 
