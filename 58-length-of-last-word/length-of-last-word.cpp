9030a8d68c1888d8964b9807f281eddb3372dd28
Manan Kapoor <23.kapoormanan@gmail.com>
2025-11-10 23:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.57%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int index = s.size()-1;
+        int index = s.size()-1; // index rn is 9
         while(index >=0 && s[index]==' '){
             index--;
         }
-        int eow = index;
+        int eow = index; // eow = 9
         while(index>= 0 && s[index]!=' '){
-            index--;
+            index--; // 9-1-1-1-1 = 4
         }
-        int lolw = eow - index;
+        int lolw = eow - index; // 9 -4 = 5
         return lolw;
         
 
