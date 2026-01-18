69dd52eaf49a08778d9e02cae93864c15b989521
Manan Kapoor <23.kapoormanan@gmail.com>
2026-01-18 11:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (19.73%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(); // number of rows
+        int m = grid[0].size(); // number of columns
+        // make a visited array 
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        vector<int> delrow={-1,0,1,0};
+        vector<int> delcol = {0,1,0,-1};
+        int tm =0;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            tm=max(tm,t);
+            
+            // visit all the neighbours up , down , left , right 
+            for(int i =0;i<4;i++){
+                int nrow=r+delrow[i];
+                int ncol = c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol >=0 && ncol <m && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},t+1});
+                    grid[nrow][ncol]=2;
+                    vis[nrow][ncol]=1;
+                }
+            }
+
+        }
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return tm;
+    }
+};
