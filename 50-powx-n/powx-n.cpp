963db0fc54f7ec830007e39aaa0a051542ae6f53
Manan Kapoor <23.kapoormanan@gmail.com>
2026-01-28 22:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.99%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+        double power(double x,int n){
+            if(n==0){
+                return 1;
+            }
+            double half = power(x,n/2);
+            if(n%2==0){
+                return half*half;
+            }
+            else{
+                return half*half*x;
+            }
+        }
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(n<0){
+            x=1/x;
+            N=-N;
+        }
+        return power(x,N);
+
+        
+    }
+};
