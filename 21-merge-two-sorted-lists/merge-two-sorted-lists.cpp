9eed2c09fab27881bf945de73b4058eef07c422d
Manan Kapoor <23.kapoormanan@gmail.com>
2026-02-06 21:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (9.07%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,38 +10,46 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1 && !list2){
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL){
+            return l2;
+        }
+        if(l2==NULL){
+            return l1;
+        }
+        if(l1 == NULL && l2==NULL ){
             return NULL;
         }
-        if(list1 && !list2){
-            return list1;
-        }
-        if(!list1 && list2){
-            return list2;
-        }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* tail = dummy ;
-        ListNode* curr1 = list1;
-        ListNode* curr2 = list2;
-        while(curr1 != NULL && curr2 != NULL){
-            if(curr1->val>curr2->val){
-                tail->next = curr2;
-                curr2=curr2->next;
-            }
-            else if(curr1->val<=curr2->val){
-                tail->next = curr1;
-                curr1=curr1->next;
-            }
-            tail=tail->next;
-        }
-        if(curr1){
-            tail->next = curr1;
+
+        ListNode* head;
+        if(l1->val<=l2->val){
+            head = l1;
+            l1=l1->next;
         }
         else{
-            tail->next = curr2;
+            head = l2;
+            l2=l2->next;
         }
-        return dummy->next;
+
+        ListNode* curr = head;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val <= l2->val){
+                curr->next = l1;
+                l1=l1->next;
+            }
+            else{
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        if(l1){
+            curr->next = l1;
+        }
+        else{
+            curr->next = l2;
+        }
+        return head;
         
     }
 };
