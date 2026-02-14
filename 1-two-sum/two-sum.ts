4469d114f9dff532bd767b9a1cd7a8ee0d3a5260
Manan Kapoor <23.kapoormanan@gmail.com>
2026-02-14 21:09:03 +0530

    Time: 48 ms (15.33%) | Memory: 56.6 MB (71.13%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,6 +1,7 @@
 function twoSum(nums: number[], target: number): number[] {
     let n = nums.length;
     for(let i =0;i<n;i++){
+        // solution
         for(let j =i+1;j<n;j++){
             if(nums[i]+nums[j]===target){
                 return [i,j];
