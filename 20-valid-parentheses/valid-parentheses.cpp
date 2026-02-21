2dabcc7e50ea5a3ba17bbe15f34208bb8f6929ac
Manan Kapoor <23.kapoormanan@gmail.com>
2026-02-21 23:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (87.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
         for(char c:s){
-            if(c=='('||c=='['||c=='{'){
+            if(c=='['||c=='{'||c=='('){
                 st.push(c);
             }
             else{
                 if(st.empty()){
+                    // matlab tumhare closing ke liye waha koi 
+                    // opening bracket nai pada hai
                     return false;
                 }
                 char top = st.top();
                 st.pop();
-                if((c==')'&&top!='(')||(c==']'&&top!='[')||(c=='}'&&top!='{')){
+                if(c==')'&& top!= '(' || c==']'&& top != '[' || c=='}'&& top != '{'){
                     return false;
                 }
             }
         }
-        return st.empty();
-        
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+
+
+
+
     }
 };
