db950c9aa0883ff8544ac7400f44a400f30519a7
Manan Kapoor <23.kapoormanan@gmail.com>
2026-03-08 22:24:40 +0530

    Time: 46 ms (14.93%) | Memory: 27.6 MB (30.44%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(string &x, string &y,int n,int m,vector<vector<int>> &dp){
+        if(n==0 || m == 0){
+            return 0;
+        }
+
+        if(dp[n][m] != -1)
+            return dp[n][m];
+
+
+        if(x[n-1]==y[m-1]){
+            return dp[n][m]= (1+solve(x,y,n-1,m-1,dp));
+        }
+        else{
+            return dp[n][m] = max(solve(x,y,n-1,m,dp),solve(x,y,n,m-1,dp));
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+        return solve(text1,text2,n,m,dp);
+        
+    }
+};
