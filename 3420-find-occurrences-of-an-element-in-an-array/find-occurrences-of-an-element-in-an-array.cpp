acbea9d6133717161c2977a5ec76aea6c97c09f8
Manan Kapoor <23.kapoormanan@gmail.com>
2026-03-26 11:58:29 +0530

    Time: 35 ms (23.31%) | Memory: 199.6 MB (5.06%) - LeetSync

---
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        int n = nums.size();
+        int q = queries.size();
+
+        // use unordered map 
+        unordered_map<int,vector<int>>mp;
+        for(int i =0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        
+        vector<int> ans;
+        // element not found in the array hence returing -1
+        if(!(mp.find(x)!=mp.end())){
+           vector<int> ret(q,-1);
+           return ret;
+        }
+        vector<int> pos = mp[x];
+        // now we have mp.first as the element , and mp.second as its occurences 
+        for(int i =0;i<q;i++){
+            if(queries[i]>pos.size()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(pos[queries[i]-1]);
+            }
+        }
+        return ans;
+
+    }
+};
