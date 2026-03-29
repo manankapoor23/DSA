27d43988cc9bc7667baff8cc6a561d63a563c8c0
Manan Kapoor <23.kapoormanan@gmail.com>
2026-03-29 23:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (79.98%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -3,9 +3,8 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for(int i =0;i<words.size();i++){
-            for(int j =0;j<words[i].size();j++){
-                string s = words[i];
-                if(s[j]==x){
+            for(char c:words[i]){
+                if(c==x){
                     ans.push_back(i);
                     break;
                 }
