d591b2c0dcc04c0cdb39a6aa1d18d46e895443a0
Manan Kapoor <23.kapoormanan@gmail.com>
2026-04-04 14:11:27 +0530

    Time: 3 ms (6.12%) | Memory: 10.7 MB (43.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int picker(vector<int> &nums,int idx,vector<int> &dp){
-        int pick_sum =0;
-        int notpick_sum =0;
-        if(idx==0){
-            //pick 0 index
-            pick_sum +=nums[0];
-            
-        }
-        if(idx<0){
-            // out of bounds 
-            return 0;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
+    int tabulation(vector<int> &nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int> dp(n);
+
+        dp[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if(i > 1) {
+                pick += dp[i-2];
+            }
+
+            int skip = dp[i-1];
+
+            dp[i] = max(pick, skip);
         }
 
-        // if we pick a[i] then we add a[i] to our sum and move to a[i-2]
-        pick_sum = nums[idx]+picker(nums,idx-2,dp);
-        // if we dont pick a[i] then we add 0 to our sum and move to a[i-1]
-        notpick_sum =picker(nums,idx-1,dp);
-        return dp[idx]=max(pick_sum,notpick_sum);
+        return dp[n-1];
     }
-    int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        int ans = picker(nums,nums.size()-1,dp);
-        return ans;
-        
 
-        
+    int rob(vector<int>& nums) {
+        return tabulation(nums);
     }
 };
