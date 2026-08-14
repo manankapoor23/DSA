6055e53a3b78a9ef3b53647a28a939a20a18a7cd
Manan Kapoor <23.kapoormanan@gmail.com>
2026-08-14 12:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (46.57%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n = s.length();
-        vector<int> dp(n+1,0);// intialise 
+    int solve(int i,string &s,vector<int> &dp){
+        // if we hae reached the end we return as 1 solved
+        int n = s.size();
+        if(i==s.size()){
+            return 1;
+        }
+        if(s[i]=='0'){
+            return 0;
+        }
+        // intialise 
         dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0'){
-                dp[i]=0;
-            }
-            else{
+        for(int i =n-1;i>=0;i--){
+            if(s[i]!='0'){
             dp[i]+=dp[i+1];
+            }
             if(i+1<n){
-                if(s[i]=='1' || (s[i]=='2'&& s[i+1]<='6')){
+                if(s[i]=='1'||(s[i]=='2'&&s[i+1]<='6')){
                     dp[i]+=dp[i+2];
                 }
-            }
+
             }
         }
         return dp[0];
+    }
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        int ans = solve(0,s,dp);
+        return ans;
         
     }
 };
