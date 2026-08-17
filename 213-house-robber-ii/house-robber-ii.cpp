381e74522fa44cd8d83d4919092a4a5d927d6ff1
Manan Kapoor <23.kapoormanan@gmail.com>
2026-08-17 14:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (52.77%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-    int picker(vector<int> &nums){
-        int pick =0;
-        int skip =0;
-        int ans =0;
+    int solve(vector<int> &nums,vector<int> &dp){
         int n = nums.size();
+        if(n<0){
+            return 0;
+        }
         if(n==0){
             return 0;
         }
-
-        vector<int> dp(n);
         dp[0]=nums[0];
-        for(int i =1;i<n;i++){
-            pick = nums[i];
-            if(i>1){
-                pick+=dp[i-2];
-            }
-            skip = dp[i-1];
-            dp[i] = max(pick,skip);
+        if(n>1){
+        dp[1]=max(nums[0],nums[1]);
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        for(int i =2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
         }
         return dp[n-1];
-
     }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
+    int rob(vector<int>& nums){
+        vector<int> first;
+        vector<int> second;
+        vector<int> dp(nums.size()+1,-1);
+        int n = nums.size();
         if(n==1){
             return nums[0];
         }
-        vector<int> vec1;
-        vector<int> vec2;
-        for(int i =0;i<nums.size();i++){
-            if(i!=0) vec1.push_back(nums[i]); // everything except phela element wala array
-            if(i!=nums.size()-1) vec2.push_back(nums[i]); // everything except last wala 
-            // now we need to picker on both of these and return max
+        for(int i =0;i<n;i++){
+            if(i!=0){
+                first.push_back(nums[i]);
+            }
+            if(i!=n-1){
+                second.push_back(nums[i]);
+            }
         }
-        return max(picker(vec1),picker(vec2));
+        int a1 = solve(first,dp);
+        int a2 = solve(second,dp);
+        return max(a1,a2);
 
-
-
-    
+        
     }
 };
