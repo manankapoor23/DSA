7a0e6dd26ab18547828cfe315d17096c9f3bb0fd
Manan Kapoor <23.kapoormanan@gmail.com>
2026-08-28 00:55:24 +0530

    Time: 3 ms (79.25%) | Memory: 14.1 MB (87.31%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int curr,int prev,int n,vector<vector<int>> &dp){
-        if(curr==n){
+    int solve(vector<int> &nums, int n){
+        // here n is the size 
+        if(n==0){
             return 0;
         }
-        if(dp[curr][prev+1]!=-1){
-            return dp[curr][prev+1];
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i =1;i<nums.size();i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                // put lower bound wali position pe
+                int index = lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
         }
-        int take =0;
-        // taking the current element only if curr>prev
-        if((prev==-1)||(nums[curr]>nums[prev] )) {
-            take = 1+solve(nums,curr+1,curr,n,dp);
-        }
-        // excluding the current element;
-        int exc=0;
-        exc=0+solve(nums,curr+1,prev,n,dp);
-        return dp[curr][prev+1]=max(exc,take);
+        return ans.size();
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        int ans = solve(nums,0,-1,nums.size(),dp);
-        return ans;
+        return solve(nums,nums.size());
     }
 };
