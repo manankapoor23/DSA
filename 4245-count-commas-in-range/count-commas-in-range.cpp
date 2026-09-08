35b6c34c11734ed2ee7f0f130d08205a8a33e4da
Manan Kapoor <23.kapoormanan@gmail.com>
2026-09-08 10:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<=999){
+            return 0;
+        }
+        if(n<=9999){
+            return n-999;
+        }
+        if(n<=100000){
+            return (9001+n-10000); 
+        }
+
+        return 0;
+
+
+
+    }
+};
